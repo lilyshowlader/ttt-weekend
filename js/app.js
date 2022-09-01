a1436f56ef936b9a043ad5d0ab1178f3f3e8d118
lilyshowlader <lilyshowlader@gmail.com>
2022-09-01 16:39:31 -0400

    step two complete

--- js/app.js
+++ js/app.js
@@ -1,18 +1,29 @@
-/*-------------------------------- Constants --------------------------------*/
+/*------------------------ Constants --------------------------------*/
 
 
 
-/*---------------------------- Variables (state) ----------------------------*/
+/*----------------------Variables (state) ----------------------------*/
+// Step 1 - Define the required variables used to track the state of the game
+
+  // 1a) Use a variable named `board` to represent the state of the squares on
+  //    the board.
+
+  // 1b) Use a variable named `turn` to track whose turn it is.
+
+  // 1c) Use a variable named `winner` to represent if anyone has won yet, or 
+  //    if a tie has occurred.
+let board, turn, winner
+
+
+/*------------------------ Cached Element References ----------------*/
+const squareEls = document.querySelectorAll(".board > div")
+console.log(squareEls)
+const messageEl = document.querySelector("#message")
+console.log(messageEl)
+
+/*--------------------------- Event Listeners -----------------------*/
 
 
 
-/*------------------------ Cached Element References ------------------------*/
-
-
-
-/*----------------------------- Event Listeners -----------------------------*/
-
-
-
-/*-------------------------------- Functions --------------------------------*/
+/*------------------------------ Functions --------------------------*/
 
